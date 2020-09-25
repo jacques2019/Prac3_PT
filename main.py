219cd4de95962ff4eb19f2e20269708b5536976e
jacques2019 <jacques@DESKTOP-C33UBO6.localdomain>
2020-09-25 15:48:30 +0200

    Make toggle

--- main.py
+++ main.py
@@ -2,19 +2,22 @@ import RPi.GPIO as GPIO
 import time
 
 def main():
-    while(true):
         pinIN = 11
         pinLED = 13
         GPIO.setmode(GPIO.BOARD)
         GPIO.setup(pinIN, GPIO.IN, pullup_down=GPIO.PUD_DOWN)
         GPIO.setup(pinLED, GPIO.OUT)
 
-        if (GPIO.input(pinIN)):
-            GPIO.output(pinLED, GPIO.HIGH)
-        else:
-            GPIO.output(pinLED, GPIO.LOW)
-        
-        time.sleep(0.01)
+        lastIN = GPIO.input(pinIN)
 
-            
-    
+    while(true):
+
+        if (GPIO.input(pinIN) != lastIN):
+            if (lastIN == 1):
+                GPIO.output(pinLED, GPIO.LOW)
+                lastIN = 0;
+            else:
+                GPIO.output(pinLED, GPIO.HIGH)
+                lastIN = 1;
+        
+        time.sleep(0.001)
